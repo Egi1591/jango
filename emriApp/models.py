86a5f5af0b97b15212d6102124695c1529437ba9
Egi1591 <egicara1991@gmail.com>
2024-07-11 13:36:13 +0200

    change

--- emriApp/models.py
+++ emriApp/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_name=models.CharField(max_length=200, null=True, blank=True)
+    category_image= models.ImageField(upload_to='item/', null=True, blank=True)
+    category_slug=models.SlugField(unique=True, null=True, blank=True)
+
+
+
 class Item(models.Model):
     item_name= models.CharField(max_length=200, null=True, blank=True)
     item_decription= models.TextField(max_length=1000, null=True, blank=True)
@@ -9,6 +16,17 @@ class Item(models.Model):
     item_karburanti=models.CharField(max_length=200, null=True, blank=True)
     item_vitiprodhimit=models.IntegerField(null=True, blank=True)
     item_image = models.ImageField(upload_to='item/', null=True, blank=True)
+    item_category= models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
+
 
     def __str__(self):
         return f"{self.item_name}"
+
+class Contact(models.Model):
+    contact_name= models.CharField(max_length=200, null=True, blank=True)
+    contact_surname= models.CharField(max_length=200, null=True, blank=True)
+    contact_email=models.EmailField(max_length=200, null=True, blank=True)
+    contact_description=models.TextField(max_length=200, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.contact_name} {self.contact_surname}"
--- emriApp/urls.py
+++ emriApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path("", views.home, name="homePage"),
-    path('about/', views.about, name="aboutPage")
+    path('about/', views.about, name="aboutPage"),
+    path("detailitem/<id>", views.detailitem, name="detailitemPage"),
+    path("detailCategory/<slug>", views.detailCategory, name="detailCategoryPage")
 ]
